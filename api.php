2c89f21ab50625dd83939483b13236b176d8d0fd
Vinicio Valbuena <formatcomvinicio@gmail.com>
2017-11-23 10:21:15 -0500

    se agrego ejemplo de insert

--- api.php
+++ api.php
@@ -2,6 +2,19 @@
 
 header('Content-Type: application/json; charset=utf8');
 
-print('[{"id":1, "name":"vinicio"}, {"id": 2, "name":"ronnel"}]');
+// Esto simula ser los datos de tu base de datos
+$data = [
+		Array('id' => 1, 'name' => 'vinicio'),
+		Array('id' => 2, 'name' => 'ronnel')
+	];
+
+// Esto simula ser un insert, tu haces con los datos lo que se
+// te de la gana
+
+if(isset($_POST['name'])) {
+	array_push($data, Array('id' => 3, 'name' => $_POST['name']));
+}
+
+print(json_encode($data));
 
 ?>
